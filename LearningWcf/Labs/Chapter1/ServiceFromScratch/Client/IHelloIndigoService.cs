22ff4f56ec125587478dca07eafbb8e333215e6d
Zeljko Tepsic <ztepsic@gmail.com>
2013-03-23 23:05:47 +0100

    Implementation of Learning WCF - Chapter 01 - ServiceFromScratch

---
+++ LearningWcf/Labs/Chapter1/ServiceFromScratch/Client/IHelloIndigoService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Client {
+	
+	[ServiceContract(Namespace = "http://www.thatindigogirl.com/samples/2006/06")]
+	public interface IHelloIndigoService {
+		[OperationContract]
+		string HelloIndigo();
+	}
+
+}
---
+++ LearningWcf/Labs/Chapter1/ServiceFromScratch/Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Client {
+	class Program {
+		static void Main(string[] args) {
+			EndpointAddress ep = new EndpointAddress("http://localhost:8000/HelloIndigo/HelloIndigoService");
+
+			IHelloIndigoService proxy = ChannelFactory<IHelloIndigoService>.CreateChannel(new BasicHttpBinding(), ep);
+			string s = proxy.HelloIndigo();
+			Console.WriteLine(s);
+			Console.WriteLine("Press <ENTER> to terminate Client.");
+			Console.ReadLine();
+		}
+	}
+}
